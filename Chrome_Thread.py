28c8a38be28b09eb24b7517a2cf99ac3d06d989e
dishbusiness <ryan.burch@dish.com>
2020-11-13 21:03:39 -0700

    DAVITA OnStream test

--- Chrome_Thread.py
+++ Chrome_Thread.py
@@ -1,10 +1,17 @@
+import os
 import subprocess
 from threading import Thread
+
+try:
+    test_path = os.environ['ONSTREAM_TEST']
+except KeyError:
+    print('Could not get environment variable "test_path". This is needed for the tests!"')
+    raise
 
 
 def pytest_run():
     while True:
-        subprocess.run(['pytest', 'OnStream_Chrome.py', '-v', '-s'])
+        subprocess.run(['pytest', os.path.join(test_path, 'OnStream_Chrome.py'), '-v', '-s'])
 
 
 if __name__ == "__main__":
